f308188f5fc06f9e433712c2708b26969e786824
Seyyed Abbas Mirghasemi <sam.s.a.mirghasemi@gmail.com>
2022-03-29 11:47:21 +0430

    q1 done!

---
+++ hw3/1/src/Timer.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import './Timer.css';
+
+// const ms = require('pretty-ms')
+
+export class Timer extends Component {
+    constructor(props){
+        super(props)
+        this.resetTimer = this.resetTimer.bind(this)
+
+        this.state = {
+            time: 0,
+        }
+
+        this.timer = setInterval(() => this.setState({
+            time: this.state.time + 1
+        }), 1000);
+    }
+
+    resetTimer() {
+        // console.log("reset")
+        this.setState({
+            time: 0
+        })
+        clearInterval(this.timer)
+        this.timer = setInterval(() => this.setState({
+            time: this.state.time + 1
+        }), 1000);
+    }
+
+    render() {
+        return <div className="container">
+            <div className="timer">
+                <p>{this.state.time}</p>
+            </div>
+            <button onClick={this.resetTimer} >Reset Timer</button>
+        </div>;
+    }
+}
---
+++ hw3/2/src/input.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {validate} from './validators';
+
+const INPUT_STATES = {
+  UNTOUCHED: 'UNTOUCHED',
+  VALID: 'VALID',
+  INVALID: 'INVALID'
+};
+
+const Input = props => {
+   return (
+     <div className='form-input' data-testid="form-input">
+       <label></label>
+       <input />
+       <p></p>
+     </div>
+   )
+};
+
+export default Input;
