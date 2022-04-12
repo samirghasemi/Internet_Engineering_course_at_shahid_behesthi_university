6f84aaaa50c90234782772e761eecc6217a3c9c3
Seyyed Abbas Mirghasemi <sam.s.a.mirghasemi@gmail.com>
2022-04-13 00:12:33 +0430

    update

--- hw3/3/src/components/IranMap.js
+++ hw3/3/src/components/IranMap.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './IranMap.css';
 
 import CityModal from './CityModal';
+import cityModal from "./CityModal";
 
 
 class IranMap extends React.Component {
@@ -13,10 +14,28 @@ class IranMap extends React.Component {
         isModalOpen: false,
     };
 
+    // constructor(props) {
+    //     super(props);
+    //
+    //     this.state = {
+    //         movies: []
+    //     };
+    // }
+
+    componentDidMount() {
+        fetch('http://localhost:9000/cities/').then(res => res.json()).then(citiesData  =>
+            this.setState({'citiesData': citiesData })
+        );
+    }
+
 
     cityClicked = (id) => (event) => {
         event.preventDefault();
         // Fetch city details and open modal
+        fetch('http://localhost:9000/cities/'+id).then(res => res.json()).then(selectedCity  =>
+            this.setState({'selectedCity': selectedCity })
+        );
+        this.state.isModalOpen = true;
     };
 
     closeModal = () => {
