f108d4533f5a12d6db5fba272aee71bc70bbe87d
Seyyed Abbas Mirghasemi <sam.s.a.mirghasemi@gmail.com>
2022-04-12 17:14:37 +0430

    q3 done!

--- hw3/2/src/input.js
+++ hw3/2/src/input.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {validate} from './validators';
 
 const INPUT_STATES = {
-  UNTOUCHED: 'UNTOUCHED',
-  VALID: 'VALID',
-  INVALID: 'INVALID'
+    UNTOUCHED: 'UNTOUCHED',
+    VALID: 'VALID',
+    INVALID: 'INVALID'
 };
 
-const Input = props => {
-   return (
-     <div className='form-input' data-testid="form-input">
-       <label>{this.label}</label>
-       <input />
-       <p></p>
-     </div>
-   )
-};
+const Input = ({
+                   type,
+                   label,
+                   id,
+                   validators,
+                   errorText,
+               }) => {
 
+    const [state , setState]=useState(INPUT_STATES.UNTOUCHED)
+    // console.log({validators,value,state})
+    // console.log(validate(value , validators))
+    return (
+        <div className={`form-input ${state===INPUT_STATES.INVALID ? 'form-input--invalid':''}` } data-testid="form-input">
+            <label htmlFor={id}>{label}</label>
+            <input type={type} id={id} onChange={(e)=> {
+                validate(e.target.value , validators) ? setState(INPUT_STATES.VALID) : setState(INPUT_STATES.INVALID);
+            }}/>
+            {state===INPUT_STATES.INVALID &&  <p>{errorText}</p>}
+        </div>
+    )
+}
 export default Input;
